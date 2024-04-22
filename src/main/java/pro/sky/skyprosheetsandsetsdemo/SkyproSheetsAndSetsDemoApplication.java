41f63a1f92a03a260ab16fc573b3c53dd76a5460
original <git@github.com:denis-poroshin/variables.git>
2024-04-22 19:47:43 +0300

    Начало

---
+++ src/main/java/pro/sky/skyprosheetsandsetsdemo/SkyproSheetsAndSetsDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprosheetsandsetsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSheetsAndSetsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSheetsAndSetsDemoApplication.class, args);
+	}
+
+}
